d115effa37a6ed7fc4430397a72eaa2f8bd1b42a
Marvin Mwendwa <mwendwamarvin09@gmail.com>
2023-02-14 12:57:07 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+
+/*let js = 'amazing';
+if (js ==='amazing') alert ('Javascript is FUN');
+40 + 8 + 23-10;
+console.log (40 + 8 + 23-10);
+console.log ("jonas");
+
+let firstName ="jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob="Teacher";
+let mySecondJob ="Programmer"
+console.log(myFirstJob);
+
+
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+//boolean
+console.log(typeof true);
+//boolean
+console.log(typeof javascriptIsFun);
+// 23 is number
+console.log(typeof 23);
+// jonas is a string  you must always quote a string
+console.log(typeof "jonas");
+
+//dynamic typing
+javascriptIsFun = 'YES!'; 
+console.log(typeof javascriptIsFun);// this now changed to a string
+*/
+//example of math operator (-)
+/*const now = 2030
+const ageMarvin = now - 1997;
+const ageNdanu = now -1999;
+console.log(ageMarvin, ageNdanu);
+
+console.log(ageMarvin * 2, ageNdanu / 10, 2 ** 3);
+
+const firstName = 'MARVIN';
+const lastName = 'MWENDWA';
+//'' is used to add space
+console.log(firstName + ' ' + lastName);
+
+//assignment operator
+let x = 10 + 5; // the console should show 15
+x += 10; //x = x + 10  answer should show 25
+x  *= 4; // x = 25*4 =100 
+x ++;
+console.log(x);
+
+//comparison operators
+console.log(ageMarvin > ageNdanu); // >, <, >=, >=.
+console.log(ageNdanu >= 18);
+const isFullAge = ageNdanu >= 18;
+console.log(now - 1997 > now -1999)*/
+
+//opertor presedence
+
+/*const massMark = 78;
+const heightMark =  1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark= massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn, markHigherBMI);*/
+
+
+const firstName = 'jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
